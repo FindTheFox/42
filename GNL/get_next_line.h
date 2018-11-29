b1eaf33298a28eaf9c26facbf4b669fe4740c2d0
Samuel NEVEU <saneveu@e1r7p5.42.fr>
2018-11-29 21:09:18 +0100

    Fillit

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: saneveu <saneveu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 18:00:46 by saneveu           #+#    #+#             */
-/*   Updated: 2018/11/21 13:05:28 by saneveu          ###   ########.fr       */
+/*   Updated: 2018/11/27 12:28:57 by saneveu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft.h"
-# define BUFF_SIZE 32
+# define BUFF_SIZE 10
 # include <unistd.h>
 
 int		get_next_line(const int fd, char **line);
--- GNL/main.c
+++ GNL/main.c
@@ -1,26 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: saneveu <saneveu@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/19 19:55:15 by saneveu           #+#    #+#             */
-/*   Updated: 2018/11/21 13:09:15 by saneveu          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <fcntl.h>
-#include <stdio.h>
 
 int		main(void)
 {
-	int fd;
 	char *line;
+	int fd;
 
 	fd = open("test", O_RDONLY);
-	while(get_next_line(fd, &line) == 1)
-	printf("%s\n", line);
+	while (get_next_line(fd, &line))
+	{
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
 	close(fd);
+	return (0);
 }
